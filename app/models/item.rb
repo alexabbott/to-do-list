e6f87f1afa5f6c2e658a631e891e6abbf23541ae
alexabbott <alex.r.abbott@gmail.com>
2014-11-18 11:15:16 -0800

    Initialize models

---
+++ app/models/item.rb
@@ -0,0 +1,3 @@
+class Item < ActiveRecord::Base
+	validates_length_of :name, minimum: 1, maximum: 50
+end
---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :a_user, class: User do
+		
+		email ""
+		password ""
+
+	end
+end
